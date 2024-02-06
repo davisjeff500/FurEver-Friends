09f917b05f7218ad6a7a8ce8b1d97ee10afc9d6c
Evan Keller <33649ek@gmail.com>
2024-02-05 23:28:36 -0700

    server now starts correctly when 'node server.js' is run in the console

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -41,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+const listeningLog = `Now listening on port ${PORT}!`;
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(listeningLog));
 });
