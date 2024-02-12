465dcc33b1c69def9bb0f6c5a13ba259a933fb6a
Corey Donahue <137344214+CDarts48@users.noreply.github.com>
2024-02-12 05:44:34 -0700

    Update users.sql

--- db/users.sql
+++ db/users.sql
@@ -7,15 +7,6 @@ CREATE TABLE users (
     userName VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    fostering BOOLEAN,
-    hasPets INT CHECK (hasPets >= 1 AND hasPets <= 4),
-    fencedYard BOOLEAN,
-    hasKids BOOLEAN,
-    previousExp INT CHECK (previousExp >= 1 AND previousExp <= 4),
-    anythingElse TEXT,
-    why TEXT,
-    createdAt TIMESTAMP,
-    updatedAt TIMESTAMP
 );
 CREATE TABLE sessions (
   sid VARCHAR(255) PRIMARY KEY,
